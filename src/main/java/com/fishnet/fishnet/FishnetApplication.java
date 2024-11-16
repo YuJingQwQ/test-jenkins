cd93c65e56b994c35daac96fe1a25991cbc018b7
fishnet <951734771@qq.com>
2024-11-16 16:04:14 +0800

    init project

---
+++ src/main/java/com/fishnet/fishnet/FishnetApplication.java
@@ -0,0 +1,13 @@
+package com.fishnet.fishnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishnetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishnetApplication.class, args);
+    }
+
+}
